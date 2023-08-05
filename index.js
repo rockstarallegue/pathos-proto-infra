ff7bf3d1ade3aac198b50e9679ec82f1716d1975
Allegue <rockstarallegue@gmail.com>
2023-08-05 00:45:52 -0600

    :duck: Published npm package version 1.0.2 | Hello function accepts name variable

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-exports.hello = () => {
-    var result = "Hello from the Dream Operator's Garage";
+exports.hello = (name = "nameless entity") => {
+    var result = "Hello from the Dream Operator's Garage, " + name;
     return result;
 }
